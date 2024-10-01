c08d50bdf668caa67175ff4d1119f65cc5adb726
udaychopade27 <udaychopade27@gmail.com>
2024-10-01 16:18:07 +0530

    script for aws instance creation

--- aws/main.tf
+++ aws/main.tf
@@ -1,9 +1,19 @@
 terraform {
     required_providers {
-        provider {
-            aws = {
-                source = "hashicorp/aws"
-            }
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0.0"
         }
     }
+}
+
+provider "aws"  {
+    region = "ap-south-1"
+}
+
+resource "
+
+resource "aws_instance" "example" {
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
 }
