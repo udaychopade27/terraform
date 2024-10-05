5e0eefbfcbaca3a09779a9d7c2d484d2c0005bd6
udaychopade27 <udaychopade27@gmail.com>
2024-10-05 13:00:29 +0530

    create script file for keywords in terraform

--- keyword/main.tf
+++ keyword/main.tf
@@ -34,7 +34,7 @@ output "selected_element" {
   value = element(var.my_list1, 1) # Returns "banana"
 }
 
-#zipmap(key, value): Creates a map from a list of keys and a list of values.
+#zipmap(key, value): Creates a map from a list of keys and a list of values.  #map is not suppoerted in upodated version
 variable "keys" {
   type    = list
   default = ["name", "age"]
@@ -48,4 +48,27 @@ variable "values" {
 output "my_map" {
   value = zipmap(var.keys, var.values) # Returns {"name" = "Alice", "age" = 30}
 }
+
+#lookup(map, key): Retrieves the value associated with a specific key in a map.
+variable "my_map" {
+  type    = map(string)
+  default = {"name" = "Alice", "age" = "30"}
+}
+
+output "value" {
+  value = lookup(var.my_map, "name") # Returns "Alice"
+}
+
+#join(separator, list): Joins the elements of a list into a single string using the specified separator.
+variable "my_list2" {
+  type    = list
+  default = ["apple", "banana", "cherry"]
+}
+
+output "joined_string" {
+  value = join(", ", var.my_list) # Returns "apple, banana, cherry"
+}
+
+##These are examples of the built-in functions available in Terraform. \
+#You can find more functions and detailed documentation in the official Terraform documentation, which is regularly updated to include new features and improvements
 
