7d7a03a2630e2d9515153c23cd061013a21ce7bd
udaychopade27 <udaychopade27@gmail.com>
2024-10-07 17:47:48 +0530

    new script files

--- modules/main.tf
+++ modules/main.tf
@@ -1,16 +1,16 @@
-terraform {
-    required_providers {
-          aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0.0"
-        }
-    }
+provider "aws" {
+    region = "us-east-1"
+}
+provider "azurerm" {
+    
+
+}
+resource "aws_instance" "example" {
+    ami = ""
+    instance_type = "t2.micro"
+  
+}
+resource "az" "name" {
+    
 }
 
-provider "aws"  {
-    region = "ap-south-1"
-}
-
-module "consul" {
-    source = "https://github.com/hashicorp/terraform-aws-consul.git"
-}
